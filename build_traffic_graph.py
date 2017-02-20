9abd85a693f028043953d0edcd142307d76fed27
Will <wwbarbo2@illinois.edu>
2017-02-20 16:16:15 -0600

    modified for linux execution

--- build_traffic_graph.py
+++ build_traffic_graph.py
@@ -5,7 +5,8 @@ import datetime as dt
 
 # get nodes
 # nodes_file = '/Users/wbarbour1/Downloads/nodes.csv'
-nodes_file = '/Users/wbarbour1/Google Drive/Classes/CEE_418/final_project/selected_nodes.csv'
+# nodes_file = '/Users/wbarbour1/Google Drive/Classes/CEE_418/final_project/selected_nodes.csv'
+nodes_file = '/sata_ssd1/multimodal_graph/selected_nodes.csv'
 with open(nodes_file, 'r') as nf:
     take_cols = [(0, long), (2, int), (3, int), (5, float), (6, float), (10, int)]
     # if using selected nodes taken from QGIS, the (X, Y) coordinates will be added at the beginning of the columns
@@ -21,7 +22,8 @@ with open(nodes_file, 'r') as nf:
     print len(nodes), "nodes loaded."
 
 # get links
-links_file = '/Users/wbarbour1/Downloads/links.csv'
+# links_file = '/Users/wbarbour1/Downloads/links.csv'
+links_file = '/sata_ssd1/multimodal_graph/links.csv'
 with open(links_file, 'r') as lf:
     reader = csv.reader(lf, delimiter=',')
     take_cols = [(1, long), (2, long), (5, float), (6, str), (7, str),
@@ -33,24 +35,37 @@ with open(links_file, 'r') as lf:
 
 year = '2013'
 day0 = dt.datetime.strptime('01/01/%s 00:00:00' % year, '%m/%d/%Y %H:%M:%S').date()
-dayf = dt.datetime.strptime('12/31/%s 23:00:00' % year, '%m/%d/%Y %H:%M:%S').date()
-hourly_data_file = '/Users/wbarbour1/Downloads/'
+dayf = dt.datetime.strptime('03/31/%s 23:00:00' % year, '%m/%d/%Y %H:%M:%S').date()
+
 
 G = nx.DiGraph(name='Multimodal Routing Graph')
 
 for n in nodes:
     G.add_node(n=n[0], attr_dict=dict(zip(nodes_h[1:], n[1:])))
-print "Nodes added."
-print nx.info(G)
-for l in links[:10000]:
+print "Nodes added.\n"
+print nx.info(G), '\n'
+
+# hourly_data_file = '/Users/wbarbour1/Downloads/'
+hourly_data_file = '/sata_ssd1/multimodal_graph/travel_times_%s.csv' % year
+with open(hourly_data_file, 'r') as hf:
+    reader = csv.reader(hf, delimiter=',')
+    take_cols = [(0, long), (1, long), (2, str), (3, float)]
+    hourly_h = reader.next()
+    hourly_h = [hourly_h[c] for c, ty in take_cols]
+    hourly = [[ty(line[c]) for c, ty in take_cols] for line in reader]
+    print len(hourly), "hourly records loaded."
+    hourly = [h for h in hourly if h[0] and h[1] in G.nodes()]
+    print len(hourly), "hourly records retained."
+
+for l in links:
     if l[0] and l[1] in G.nodes():
         d = {day0 + dt.timedelta(days=i): {h: 10. for h in range(0, 24)} for i in range((dayf - day0).days)}
         tve = TVEdge(data_map=d, tv_type='day:hour')
         G.add_edge(u=l[0], v=l[1], attr_dict=dict(zip(links_h[2:], l[2:])), tve=tve)
-print "Links added."
-print nx.info(G)
+print "Links added.\n"
+print nx.info(G), '\n'
 
-nx.write_gpickle(G, '/Users/wbarbour1/Downloads/graph_test1.gpkl')
-
-# Ideas:
-#   - use string or int for month:date:hour key in TVE
+# graph_dest_path = '/Users/wbarbour1/Downloads/graph_test1.gpkl'
+graph_dest_path = '/sata_ssd1/multimodal_graph/graph_test_full.gpkl'
+nx.write_gpickle(G, graph_dest_path)
+print "Graph written to file:", graph_dest_path
